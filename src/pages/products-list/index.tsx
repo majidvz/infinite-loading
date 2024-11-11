0942b6613822b8f0be72d7c6767148698a1fa95e
Majid Valizadeh <33004372+majidvz@users.noreply.github.com>
2024-11-11 17:24:40 +0330

    initialling project

---
+++ src/pages/products-list/index.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+import { ProductsListWrapper } from "./styles";
+
+export const ProductsList: FC = () => {
+  return <ProductsListWrapper>ProductsList</ProductsListWrapper>;
+};
---
+++ src/pages/products-list/styles.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+
+export const ProductsListWrapper = styled.div``;
